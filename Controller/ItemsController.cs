26ba618d6e818249b93577b180bf1079dab30d0d
Михаил Казаков <mishakazakov82@gmail.com>
2026-05-05 10:32:10 +0500

    Разработанный контроллер предметов. Казаков

---
+++ Controller/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+
+namespace Shop.Controller
+{
+    public class ItemsController
+    {
+        public IItems IAllItems;
+        private ICategorys IAllCategorys;
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategorys = IAllCategorys;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+
+            var cars -IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Data.Interfaces;
+using Shop.Data.Mocks;
 
 namespace Shop
 {
@@ -16,7 +18,10 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddTransient<ICategorys, MockCaregorys>();
+            services.AddTransient<IItems, MockItems>();
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

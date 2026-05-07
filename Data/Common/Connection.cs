a93107822837f0d07635b2758dd15ef6a2c40d87
anekk <aneckalarionova@gmail.com>
2026-05-07 09:09:07 +0500

    Разработанное подключение к MySql.  Казаков

---
+++ Data/Common/Connection.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Shop.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnData = "server=localhost;port=3306;database=Shop;uid=root;pwd=;";
+
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection conn = new MySqlConnection(ConnData);
+            conn.Open();
+
+            return conn;
+        }
+
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection connection)
+        {
+            MySqlCommand cmd = new MySqlCommand(Query, connection);
+            return cmd.ExecuteReader();
+        }
+
+        public static void MySqlClose(MySqlConnection connection) {
+            connection.Close();
+    }
+}

39e3a87adb0a5cd8953c0a2da4c252efcb46f6d6
Михаил Казаков <mishakazakov82@gmail.com>
2026-05-14 13:08:06 +0500

    Разработанное перенаправление на страницу с предметами.  Казаков

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class HomeController
+    {
+        public RedirectResult Index()
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
